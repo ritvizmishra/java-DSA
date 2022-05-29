13392381a465fc395b03179c24c395bda97c542b
Ritviz <1822mritviz@gmail.com>
2022-05-29 14:36:36 +0530

    Rotated binary search in Array with duplicate values.

---
+++ Binary  Search/src/CountRotation.java
@@ -0,0 +1,25 @@
+public class CountRotation {
+    static int rotationCount(int[] nums){
+        int max = nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+        int i = 0;
+        while (i < nums.length) {
+
+            if (nums[i] == max) {
+                return i+1;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {24,69,100,99,79,78,67,36,26,19};
+        System.out.println(rotationCount(nums));
+    }
+}
