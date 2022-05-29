a405088a8cc5f128795639cde284d5b687afbc94
Ritviz <1822mritviz@gmail.com>
2022-05-30 01:00:33 +0530

    Bubble Sort

--- Sorting/Bubble Sort/src/BubbleSort.java
+++ Sorting/Bubble Sort/src/BubbleSort.java
@@ -7,13 +7,19 @@ public class BubbleSort {
         System.out.println(Arrays.toString(arr));
     }
     static void bubble(int[] arr){
+        boolean swapped;
         for(int i = 0; i < arr.length; i++){
+            swapped = false;
             for(int j = 1; j < arr.length - i; j++){
                 if (arr[j] < arr[j-1]) {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
+            }
+            if(!swapped){
+                break;
             }
         }
     }
